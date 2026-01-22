b739c3a059d8ad8eb8ab4e5cac6080873fcad038
karim <karimalaa298@gmail.com>
2026-01-22 10:52:46 +0300

    map dummy modal

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,5 +1,21 @@
+import { useContext } from "react";
+import { CartContext } from "../context/CartContext.jsx";
+
 export default function Cart(){
+    
+    const cartCtx = useContext(CartContext);
+    const items = cartCtx.items;
+
+    console.log('Cart items: ');
+    console.log(items);
+
     return(
-        <h1>Cart Items Goes Here</h1>
+        <ul>
+            {items.map((item) => (
+                <li key={item.id}>
+                    {item.name} - Quantity: {item.quantity}
+                </li>
+            ))}
+        </ul>
     );
 }
